027f4ac26b3ad771968dd1a697205c4fbd37761a
Ilia_Pavlov <pavlov_i_v@bk.ru>
2020-12-25 00:31:52 +0300

    hw2

--- HW2.1/LoginViewController.swift
+++ HW2.1/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var loginTextField: UITextField!
@@ -26,10 +26,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        let result =  checkUserCredentials()
+        if !result {
+            showAlert()
+        }
+        return result
+    }
+    
     @objc func keybosrdWillShow(notification: Notification) {
         guard let kbSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {return}
         let insets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.size.height, right: 0)
         scrollView.contentInset = insets
+    }
+    
+    func checkUserCredentials() -> Bool {
+        return loginTextField.text! == "admin" && paswordTextField.text! == "123"
+    }
+    func showAlert () {
+        let alert = UIAlertController(title: "Error", message: "Введён неверный логин или пароль", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
     }
 
     @objc func keybosrdWillHide(notification: Notification) {
@@ -43,6 +61,8 @@ class ViewController: UIViewController {
     
     @IBAction func didTabButton(_ sender: UIButton) {
     }
+    
+    
     
 }
 
